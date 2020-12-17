b8bd03f3e4d19e27ba974f6a20c36912addd8d9e
Dmitry-Poltaradnev <poltaradnev@gmail.com>
2020-12-17 17:08:43 +0300

    Add bonus for update fire mode, creation prefab bonus.

---
+++ 2d shooter/Assets/Scripts/Bonus.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bonus : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //При столкновении с игроком
+        if (collision.tag == "Player")
+        {
+            //Проверяем режим стрельбы, если он не максимален.
+            if (PlayerShooting.instance.cur_Power_Level_Guns < PlayerShooting.instance.max_Power_Level_Guns)
+            {
+                //Если он не максимален, мы меняем режим стрельбы.
+                PlayerShooting.instance.cur_Power_Level_Guns++;
+            }
+            // И впоследствии уничтожаем наш бонус.
+            Destroy(gameObject);
+        }
+    }
+}
--- 2d shooter/Assets/Scripts/PlanetsAndBonus.cs
+++ 2d shooter/Assets/Scripts/PlanetsAndBonus.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class PlanetsAndBonus : MonoBehaviour
 {
+    //Добавим переменную для хранения prefab bonus.
+    public GameObject obj_Bonus;
+    //Необходимо добавить задержку между генерациями бонусов.
+    public float time_Bonus_Spawn;
+
+
     // создаём массив с предметами для генерации.
     public GameObject[] obj_Planets;
 
@@ -18,8 +24,25 @@ public class PlanetsAndBonus : MonoBehaviour
 
     private void Start()
     {
+        //Запускаем генерацию бонусов.
+        StartCoroutine(BonusCreation());
+
+
         //Запускаем генерацию планет
         StartCoroutine(PlanetsCreation());
+    }
+    IEnumerator BonusCreation()
+    {
+        //Генерация бонусов.
+        while (true)
+        {
+            //Создаём задержку после которой будет выполнять код.
+            yield return new WaitForSeconds(time_Bonus_Spawn);
+
+            //Создаём бонус с условием ограничения движения игрока, и выше видимости камеры.
+            Instantiate(obj_Bonus, new Vector2(Random.Range(MovePlayer.instanse.borders.minX, MovePlayer.instanse.borders.maxX),
+                MovePlayer.instanse.borders.maxY * 1.5f), Quaternion.identity);
+        }
     }
 
     IEnumerator PlanetsCreation()// добавляем планеты в список используя цикл.
