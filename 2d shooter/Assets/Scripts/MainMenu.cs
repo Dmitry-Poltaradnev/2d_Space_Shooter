2ae707a318515359de4fad75ca59ec9d4db857ba
Dmitry-Poltaradnev <poltaradnev@gmail.com>
2021-02-03 17:28:16 +0300

    Added the ability to select menu panels, prefabs player ship.

---
+++ 2d shooter/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    //Добавим массив для хранения панелей Menu(Level panel, Shop panel, Upgrade panel)
+    public GameObject[] game_Panels;
+
+    //Добавим метод который будет который будет отображать панель.
+    public void Show_Change_Panel(int index)
+    {
+        game_Panels[index].SetActive(true);
+    }
+    //Добавим метод для сокрытия панели.
+    public void Hide_Change_Panel(int index)
+    {
+        game_Panels[index].SetActive(false);
+    }
+
+}
